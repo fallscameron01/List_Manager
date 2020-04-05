6b5c33610359c49b98cc66e660124be20869ad15
Cameron Falls <camfalls0201@gmail.com>
2020-04-05 14:11:08 -0500

    Update task

--- source/Task.cpp
+++ source/Task.cpp
@@ -1,1 +1,32 @@
 #include "Task.h"
+
+// GENERAL STORAGE FORMATTING
+/*
+  string content line
+  short priority
+  short status
+  int number of subtasks
+  subtasks
+*/
+istream& operator>>(istream& in, Task task)
+{
+  short temp;
+  int num_subtasks;
+
+  getline(in, task.m_content);
+  in >> task.m_priority;
+  in >> temp;
+  task.m_status = static_cast<Status>(temp);
+  
+  in >> num_subtasks;
+  
+  for (int i = 0; i < num_subtasks; i++)
+  {
+    Task sub;
+    in >> sub;
+
+    task.m_subtasks.push_back(sub);
+  }
+
+  return in;
+}
--- source/Task.h
+++ source/Task.h
@@ -1,11 +1,18 @@
 #ifndef TASK_H
 #define TASK_H
 
+#include <vector>
+using std::vector;
+
 #include "Item.h"
 
 class Task :
   public Item
 {
+  private:
+    vector<Item> m_subtasks;
+  public:
+    friend istream& operator>>(istream& in, Task task);
 };
 
 #endif // !TASK_H
