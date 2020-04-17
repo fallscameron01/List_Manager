e6de59aebcdaa7f593678859a2959ae172f54fba
Cameron Falls <camfalls0201@gmail.com>
2020-04-17 12:40:36 -0500

    update main

--- main.cpp
+++ main.cpp
@@ -6,15 +6,33 @@ int main()
   vector<TaskGroup> groups;
 
   int listChoice = 0;
+  int taskChoice = 0;
 
   loadLists(groups);
 
-  while (listChoice < 1 || listChoice >(groups.size() + 1))
+  while (listChoice != (groups.size() + 2)) // main loop, while quit not selected
   {
-    listChoice = menuLists(groups);
+    listChoice = 0;
+    taskChoice = 0;
 
-    if (listChoice < 1 || listChoice >(groups.size() + 1))
-      cout << "\nINVALID INPUT: Please try again." << endl;
+    while (listChoice < 1 || listChoice > (groups.size() + 2)) // Selecting a list
+    {
+      listChoice = menuLists(groups);
+
+      if (listChoice < 1 || listChoice > (groups.size() + 2))
+        cout << "\nINVALID INPUT: Please try again." << endl;
+    }
+
+    cout << endl;
+
+    if (listChoice < (groups.size() + 1)) // existing list
+    {
+      taskChoice = menuTasks(groups, listChoice);
+    }
+    else if (listChoice == (groups.size() + 1)) // create new list
+    {
+
+    }
   }
 
   return 0;
