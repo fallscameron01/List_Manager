f37098ec1343aa00354042d88dfc21e92494e7ce
Cameron Falls <camfalls0201@gmail.com>
2020-04-16 20:58:04 -0500

    Update main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include "source/TaskGroup.h"
+#include "source/Manager.h"
 
 int main()
 {
+  vector<TaskGroup> groups;
 
+  loadLists(groups);
+
+  for (auto& g : groups)
+    cout << g << endl;
 
   return 0;
 }
