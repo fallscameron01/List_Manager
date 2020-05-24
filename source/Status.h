532797a4d4d23a09a803ad39b0a17946506fa988
Cameron Falls <camfalls0201@gmail.com>
2020-05-24 16:18:30 -0500

    Update Status enum

--- source/Status.h
+++ source/Status.h
@@ -7,14 +7,23 @@
 #ifndef STATUS_H
 #define STATUS_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 // The Status enumeration represents the status of a task.
 enum Status
 {
-  finished = 0, // task is finished
-  todo = 1, // task is to be done in the future
-  inprogress = 2, // task is in progress
-  upnext = 3, // the next task to start
-  postponed = 4 // task has been put off
+  FINISHED = 0, // task is finished
+  TODO = 1, // task is to be done in the future
+  IN_PROGRESS = 2, // task is in progress
+  UP_NEXT = 3, // the next task to start
+  POSTPONED = 4 // task has been put off
 };
+
+ostream& operator<<(ostream& out, const Status& status);
+
+istream& operator>>(istream& in, Status& s);
 
 #endif // !STATUS_H
